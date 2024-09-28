ad41f074310fffad6bc75a5b3ad459388a4358c4
Dheerendra2003 <dheerendrac609@gmail.com>
2024-09-28 10:56:48 +0530

    first commit

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = ({ w, h }) => {
+  return (
+    <div>
+      <h1 className="text-xl/8 	font-weight: 900;">ShopSprint</h1>
+      <p className="ml-10"> </p>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ frontend/src/helpers/scrollTop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const scrollTop = () => {
+  return <div>scrollTop</div>;
+};
+
+export default scrollTop;
